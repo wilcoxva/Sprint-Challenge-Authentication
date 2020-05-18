4ec8f87dd3936ad550773312c0afc8dd41defb7c
Virginia Wilcox <wilcoxva@gmail.com>
2020-05-17 21:38:12 -0400

    passing tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -2,18 +2,30 @@ const app = require("./auth-router");
 const supertest = require('supertest')
 const request = supertest(app)
 
-describe("Auth Router test units", () => {
-    let item1;
-    beforeEach(() => {
-        item1 = {
-            "username": "wilcoxva",
-            "password": "1234"
-        }
+describe("Auth Router Register test units", () => {
+
+    it("returns json", () => {
+        request.post('/api/auth/register').send({ "username": "wilcoxva", "password": "1234" }).expect('Content-Type', /json/).end();
+
+          
     })
 
-    it("/register", async () => {
-        const response = await request.post('/api/auth/register')
+    it("expects 201", () => {
+        request.post('/api/auth/register').send({ "username": "asdf", "password": "1234" }).expect(201).end();
 
-        expect(response.status).toBe(201)
+    })
+})
+
+describe("Auth Router Login test units", () => {
+
+    it("returns json", () => {
+        request.post('/api/auth/login').send({ "username": "wilcoxva", "password": "1234" }).expect('Content-Type', /json/).end();
+
+          
+    })
+
+    it("expects 400", () => {
+        request.post('/api/auth/login').send({ "username": "asdf", "password": "1234" }).expect(400).end();
+
     })
 })
